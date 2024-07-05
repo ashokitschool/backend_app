e9bccedd0b946f1c98496373a0e1b2450395a0ba
Ashok <ashokit@gmail.com>
2024-07-05 11:19:18 +0530

    new method added to fetch orders

--- src/main/java/in/ashokit/repository/OrderRepository.java
+++ src/main/java/in/ashokit/repository/OrderRepository.java
@@ -1,12 +1,17 @@
 package in.ashokit.repository;
 
 import in.ashokit.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @RepositoryRestResource
 @CrossOrigin("http://localhost:4200")
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    public Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
 }
